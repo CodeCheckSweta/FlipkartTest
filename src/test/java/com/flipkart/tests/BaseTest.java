1f59de352666f7d808702eab59b9c8156aa85568
CodeCheckSweta <sweta.singh0322@gmail.com>
2024-10-09 11:00:48 +0530

    updated execution flow

--- src/test/java/com/flipkart/tests/BaseTest.java
+++ src/test/java/com/flipkart/tests/BaseTest.java
@@ -1,15 +1,31 @@
 package com.flipkart.tests;
 
+import java.lang.reflect.Method;
+
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import com.flipkart.driver.Driver;
+import com.flipkart.reports.ExtentReport;
 
 public class BaseTest {
 	protected BaseTest() {}
 	
+	@BeforeSuite
+	public void initialize() {
+		ExtentReport.initReports();
+	}
+	
+	@AfterSuite
+	public void flush() {
+		ExtentReport.flushReports();
+	}
+	
 	@BeforeMethod
-	protected void setUp() throws Exception {
+	protected void setUp(Method method) throws Exception {
+		ExtentReport.createTest(method.getName());
 		Driver.initDriver();
 	}
 	
